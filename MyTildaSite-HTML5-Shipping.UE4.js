3d46b8f3577bbae3f7f66d2ca1fc7ab9de6a191e
grahorica <134962063+grahorica@users.noreply.github.com>
2024-04-26 21:28:22 +0300

    Update MyTildaSite-HTML5-Shipping.UE4.js

--- MyTildaSite-HTML5-Shipping.UE4.js
+++ MyTildaSite-HTML5-Shipping.UE4.js
@@ -1154,7 +1154,7 @@ $(document).ready(function() {
 
 		// ----------------------------------------
 		// MORE JS
-		var dataJsDownload = fetchOrDownloadAndStore(db, Module.locateFile('MyTildaSite-HTML5-Shipping.data.js'));
+		var dataJsDownload = fetchOrDownloadAndStore(db, Module.locateFile('https://rawcdn.githack.com/grahorica/gameintilda/226845a8843fbbea987545d6d280f4b087b2c29c/MyTildaSite-HTML5-Shipping.data.js'));
 		var utilityJsDownload = fetchOrDownloadAndStore(db, Module.locateFile('Utility.js')).then(addScriptToDom);
 		var dataDownload =
 /* // The following code would download and store the .data file as a Blob, which should be more efficient than loading an ArrayBuffer. However that seems to be buggy, so avoid it for now.
